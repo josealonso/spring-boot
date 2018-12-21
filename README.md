01daba91ca56835df4ed045d6351d028f6e40a2d
José Ramón Alonso <jose.alonso.programmer@gmail.com>
2018-12-21 12:42:59 +0100

    Explained some basic things about Spring-Boot.

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # spring-boot
+
+### File Structure
+
+*mvnw* files allows you to run a Maven project without having Maven installed on your computer.
+
+
+#### POM File
+
+Spring Boot Starters: collection of Maven dependencies grouped together.
+  Example: spring-boot-starter-web.
+  There are 30+ Spring Boot Starters from the Spring Development team.
+  There is a Spring Boot Starter Parent in the pom.xml file.
+  spring-boot-starter-* dependendies inherit version from parent.
+Spring Boot Maven plugin: for packaging and for running 
+
--- src/main/java/com/jose/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/jose/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -3,11 +3,18 @@ package com.jose.springboot.demo.mycoolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication   
+/* 
+ * Composed of the following annotations: @EnableAutoConfiguration, @ComponentScan, @Configuration 
+ */
+// In Spring Boot, you have recursive component scanning. If you have several base packages, you must use
+// the "scanBasePackages" attribute as an argument of @SpringBootApplication.
+// Load data from application.properties
+// Do not use the src/main/webapp directory if your application is packaged as JAR.
 public class MycoolappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MycoolappApplication.class, args);
+		SpringApplication.run(MycoolappApplication.class, args);  // Bootstrap the Spring Boot application
 	}
 
 }
