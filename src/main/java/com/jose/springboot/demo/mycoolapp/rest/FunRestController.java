3929532126eb27710ffff9f5c11e6437d8746d87
José Ramón Alonso <jose.alonso.programmer@gmail.com>
2018-12-28 16:40:07 +0100

    Created a simple endpoint to inject custom application properties.

--- src/main/java/com/jose/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/jose/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -2,12 +2,28 @@ package com.jose.springboot.demo.mycoolapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
 	
+	// inject properties for: coach.name and team.name
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+	
+	// expose new endpoint for "teaminfo"
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + ", Team name: " + teamName;
+	}
+	
+	//
 	@GetMapping("/")
 	public String sayHello() {
 		return "Hello World! Time on server is " + LocalDateTime.now();
